93f9cef4883b435cd685a508f2b393c041195f4e
GCaringi <giannicaringi94@gmail.com>
2022-05-30 15:35:54 +0200

    Add bonus

--- js/main.js
+++ js/main.js
@@ -4,12 +4,12 @@ const app = new Vue({
         randomMail: [],
     },
     created() {
-            for (let i = 0; i < 10; i++){
-                axios.get("https://flynn.boolean.careers/exercises/api/random/mail")
-                .then((elem) => {
-                    console.log(elem)
-                    this.randomMail.push(elem);
-                })
-            }
-        }    
+        const interval = setInterval(() => {
+            axios.get("https://flynn.boolean.careers/exercises/api/random/mail")
+            .then((elem) => {
+                console.log(elem)
+                this.randomMail.push(elem);
+            })
+        }, 2000);
+    }    
 });
