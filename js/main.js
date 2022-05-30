d60ceb192f5bdf3bc6544643b6c1f9c4df887cbf
GCaringi <giannicaringi94@gmail.com>
2022-05-30 15:31:11 +0200

    Add call to API

--- js/main.js
+++ js/main.js
@@ -1,5 +1,15 @@
 const app = new Vue({
     el: '#app',
     data: {
-    }
+        randomMail: [],
+    },
+    created() {
+            for (let i = 0; i < 10; i++){
+                axios.get("https://flynn.boolean.careers/exercises/api/random/mail")
+                .then((elem) => {
+                    console.log(elem)
+                    this.randomMail.push(elem);
+                })
+            }
+        }    
 });
